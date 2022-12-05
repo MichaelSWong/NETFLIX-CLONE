ba58ee14c9ac6810dce30ae395a94946210da56c
Michael Wong <mizik_jah@yahoo.com>
2022-12-05 11:24:59 -0500

    added routing for pages

--- netflix-ui/src/App.tsx
+++ netflix-ui/src/App.tsx
@@ -1,7 +1,26 @@
-import React from 'react';
+import { useRoutes } from 'react-router-dom';
+import Login from './pages/Login';
+import Netflix from './pages/Netflix';
+import Signup from './pages/Signup';
 
 const App = () => {
-  return <div>App</div>;
+  const loginRoutes = {
+    path: 'login',
+    element: <Login />,
+    children: [],
+  };
+  const signupRoutes = {
+    path: 'signup',
+    element: <Signup />,
+    children: [],
+  };
+  const netflixRoutes = {
+    path: '/',
+    element: <Netflix />,
+    children: [],
+  };
+  const routing = useRoutes([loginRoutes, signupRoutes, netflixRoutes]);
+  return <>{routing}</>;
 };
 
 export default App;
